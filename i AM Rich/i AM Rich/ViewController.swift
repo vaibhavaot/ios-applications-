c5fa52e3d511438aeec48ab7a0ca53500c344e48
Vaibhav Mhatre <vaibhavaot@gmail.com>
2019-03-01 17:21:24 -0500

    Add files via upload

---
+++ i AM Rich/i AM Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i AM Rich
+//
+//  Created by Mhatre    on 2/22/19.
+//  Copyright © 2019 Mhatre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
